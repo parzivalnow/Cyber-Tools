b6a8979080c335ad30a2d60f13c7e8326049dace
unknown <parzivalnow@gmail.com>
2021-03-16 15:20:31 -0400

    Create GenerateMACAddresses.py

---
+++ GenerateMACAddresses.py
@@ -0,0 +1,18 @@
+import random
+def pad(string, length):
+    while len(string) < length:
+        string = "0" + string
+    return string
+def generateMACaddresses(amount, eui48 = True, padding = True):
+    macs = []
+    extendedLength = 0 if eui48 == True else 2
+    for x in range(amount):
+        mac = ""
+        for x in range(6+extendedLength):
+            for y in range(2):
+                if padding == True:
+                    mac += pad(hex(random.randint(0,32))[2:], 2) + ":"
+                else:
+                    mac += hex(random.randint(0,32))[2:] + ":"
+        macs.append(mac[:-1])
+    return macs
